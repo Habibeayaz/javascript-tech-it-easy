10422eaa66a5e242a2d5bf7446ae372ba5339ea6
Habibeayaz <habibe_ayaz@hotmail.com>
2023-02-13 13:08:15 +0100

    uitwerkingen

--- main.js
+++ main.js
@@ -172,5 +172,29 @@ console.log(tvSort);
 const soldOut = inventory.filter ((tvSoldout) => {
   return tvSoldout.originalStock === tvSoldout.sold;
 });
+console.log(soldOut);
 
 /// Opdracht 1c ///
+const tvAmbilight = inventory.filter ((tvAmbilight) => {
+  return tvAmbilight.options.ambiLight;
+});
+console.log(tvAmbilight);
+
+// Opdracht 1d ///
+/*function sortPrice(Array) {
+  inventory.sort((a, b) =>
+  {
+    if (a.price > b.price)
+  return 1
+    else if (a.price < b.price) {
+  return -1
+} else {
+  return 0
+});
+  }
+console.log(inventory);*/
+
+inventory.sort((a, b) => (a.price > b.price ? 1 : -1));
+console.log(inventory);
+
+// Opdracht 2a //
